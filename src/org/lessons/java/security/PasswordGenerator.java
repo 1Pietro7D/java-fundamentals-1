ebf6fbaea32a1a875f33b2b0ac4ca7603b49fc28
PietroDanieli <1pie7d@gmail.com>
2023-01-25 16:10:08 +0100

    add Date Of Birth in input and print the ''password''

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -3,43 +3,48 @@ package org.lessons.java.security;
 import java.util.Scanner;
 
 public class PasswordGenerator {
-	
+
 	static String name;
 	static String lastname;
 	static String favoriteColor;
 	static short day_of_birth;
 	static short month_of_birth;
 	static short year_of_birth;
-	//DOB contains the single variables for the date
-	//Short[] DOB = {day_of_birth,month_of_birth,year_of_birth};
 
-		public static void main(String[] args) {
-		/*
-		year_of_birth=2000;
-		Short[] DOB = {day_of_birth,month_of_birth,year_of_birth};
-		System.out.println(DOB[2]); // first test */
-		//SCANNER
-		Scanner sc= new Scanner(System.in);
+	public static void main(String[] args) {
+		// SCANNER
+		Scanner sc = new Scanner(System.in);
 		System.out.print("name?");
 		name = sc.nextLine();
 		System.out.print("lastname?");
 		lastname = sc.nextLine();
 		System.out.print("favourite color?");
 		favoriteColor = sc.nextLine();
+		System.out.print("day of birth?");
+		day_of_birth = sc.nextShort();
+		System.out.print("month of birth?");
+		month_of_birth = sc.nextShort();
+		System.out.print("year of birth?");
+		year_of_birth = sc.nextShort();
 		sc.close();
-		System.out.println(name+"-"+lastname+"-"+favoriteColor);
+		Short[] DOB = { day_of_birth, month_of_birth, year_of_birth };
+		System.out.print(name + "-" + lastname + "-" + favoriteColor);
+		for (short n : DOB) {
+			System.out.print("-");
+			System.out.print(n);			
+		}
+		System.out.println("");
+
 	}
 }
 
 /*
-Il programma deve fare quanto segue:
-1-salvare in opportune variabili il nome, cognome, colore preferito e data 
-di nascita di un utente suddivisa in giorno, mese e anno 
-in numero generare (e stampare a video) una password concatenando nome,
-cognome, colore preferito e somma di giorno, mese e anno di nascita, separate
-dal carattere -
-Esempio: ho un utente che si chiama Pinco Pallo, nato il 12/05/1994, il 
-cui colore preferito è il magenta La sua password sarà Pinco-Pallo-magenta-
-2011
-
-*/
+ * Il programma deve fare quanto segue: 1-salvare in opportune variabili il
+ * nome, cognome, colore preferito e data di nascita di un utente suddivisa in
+ * giorno, mese e anno in numero generare (e stampare a video) una password
+ * concatenando nome, cognome, colore preferito e somma di giorno, mese e anno
+ * di nascita, separate dal carattere - Esempio: ho un utente che si chiama
+ * Pinco Pallo, nato il 12/05/1994, il cui colore preferito è il magenta La sua
+ * password sarà Pinco-Pallo-magenta- 2011
+ * 
+ */
