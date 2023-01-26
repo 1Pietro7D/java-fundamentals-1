84a669426f060425556258d88303a8409786fca8
PietroDanieli <1pie7d@gmail.com>
2023-01-26 17:15:57 +0100

    translate to english

--- src/Gen26/Palindromo.java
+++ src/Gen26/Palindromo.java
@@ -4,36 +4,33 @@ public class Palindromo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Inizializziamo una variabile maxPalindrome con valore 0
+		//Initialize a variable maxPalindrome with value 0
 		int maxPalindrome = 0;
 
-		// ciclo esterno che va da 999 fino a 100 (in ordine decrescente)
+		//Outer loop that goes from 999 to 100 (in descending order)
 		for (int i = 999; i >= 100; i--) {
-			// ciclo interno che va da i fino a 100 (in ordine decrescente)
-			for (int j = i; j >= 100; j--) {
-				// calcoliamo il prodotto di i*j
-				int product = i * j;
-				// convertiamo il prodotto in una stringa
-				String productString = Integer.toString(product);
-				// creiamo un oggetto StringBuilder a partire dalla stringa del prodotto
-				StringBuilder productBuilder = new StringBuilder(productString);
-				// se la stringa del prodotto è uguale alla sua versione invertita (palindromo)
-				if (productString.equals(productBuilder.reverse().toString())) {
-					// aggiorniamo la variabile maxPalindrome con il valore massimo tra il suo
-					// valore attuale e il prodotto
-					maxPalindrome = Math.max(maxPalindrome, product);
-					// esci dal ciclo interno
-					break;
-				}
-			}
-			// se maxPalindrome è maggiore di i*999 esci dal ciclo esterno
-			if (maxPalindrome > i * 999)
-				break;
+		    //Inner loop that goes from i to 100 (in descending order)
+		    for (int j = i; j >= 100; j--) {
+		        //Calculate the product of i*j
+		        int product = i * j;
+		        //Convert the product to a string
+		        String productString = Integer.toString(product);
+		        //Create a StringBuilder object from the product string
+		        StringBuilder productBuilder = new StringBuilder(productString);
+		        //If the product string is equal to its reversed version (palindrome)
+		        if (productString.equals(productBuilder.reverse().toString())) {
+		            //Update the maxPalindrome variable with the maximum value between its current value and the product
+		            maxPalindrome = Math.max(maxPalindrome, product);
+		            //Exit the inner loop
+		            break;
+		        }
+		    }
+		    //If maxPalindrome is greater than i*999, exit the outer loop
+		    if (maxPalindrome > i * 999) break;
 		}
 
-		// stampiamo il risultato
-		System.out
-				.println("Il più grande palindromo ottenuto dal prodotto di due numeri a 3 cifre è: " + maxPalindrome);
+		//Print the result
+		System.out.println("The largest palindrome made from the product of two 3-digit numbers is: " + maxPalindrome);
 	}
 
 }
