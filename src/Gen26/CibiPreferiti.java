c8887b2ab1b7a04e01598e29fce18943423ecec6
PietroDanieli <1pie7d@gmail.com>
2023-01-26 15:31:25 +0100

    print some value of favoriteFood[]

---
+++ src/Gen26/CibiPreferiti.java
@@ -0,0 +1,18 @@
+package Gen26;
+
+public class CibiPreferiti {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] favoriteFood = {"pane", "carne", "pesce", "formiche", "coccodrilli"};
+		
+		System.out.println("favorite food" + favoriteFood[0]); // top food
+		
+		System.out.println("worst food" + favoriteFood[favoriteFood.length - 1]); // worst food
+		
+		System.out.println("middle food" + favoriteFood[favoriteFood.length / 2]); // middle food
+		
+		
+	}
+
+}
