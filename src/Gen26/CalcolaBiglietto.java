85ce17128fc25ce9eca2279fc8f142ab89da5249
PietroDanieli <1pie7d@gmail.com>
2023-01-26 15:22:19 +0100

    I initialize var discount and with conditions I give it a value to then print the total discounted price on the screen

--- src/Gen26/CalcolaBiglietto.java
+++ src/Gen26/CalcolaBiglietto.java
@@ -3,9 +3,7 @@ package Gen26;
 import java.util.Scanner;
 
 public class CalcolaBiglietto {
-	
-	
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
@@ -14,32 +12,42 @@ public class CalcolaBiglietto {
 		System.out.print("how old are you? -> ");
 		int age = Integer.parseInt(sc.nextLine());
 		sc.close();
-		
+
 		double priceTicket = 0.21d;
+		int sale20= 20;
+		int sale40= 40;
+		double totalPrice =  priceTicket * userKm;
+		double discountPrice =0 ;
+
+		if (age < 18)
+		discountPrice = totalPrice *  sale20 / 100;
+		else if (age>65)
+		discountPrice = totalPrice *  sale40 / 100;
+			
+		totalPrice = totalPrice - discountPrice;
 		
-		System.out.println(userKm);
-		System.out.println(age);
-		System.out.println(priceTicket);
-		
-		
+		System.out.println("il costo del tuo biglietto è di " + String.format("%.2f", totalPrice) + "€");
 	}
 
 }
 
 /*
  * 
- * Biglietto del treno: Il programma dovrà chiedere all’utente il numero di chilometri 
+ * Biglietto del treno: Il programma dovrà chiedere all’utente il numero di
+ * chilometri
  * 
- * che vuole percorrere e l’età del passeggero. Sulla base di queste informazioni dovrà
+ * che vuole percorrere e l’età del passeggero. Sulla base di queste
+ * informazioni dovrà
  * 
- *  calcolare il prezzo totale del viaggio, secondo queste regole: il prezzo del biglietto
- *  
- *   è definito in base ai km (0.21 € al km) va applicato uno sconto del 20% per i minorenni
- *   
- *    va applicato uno sconto del 40% per gli over 65 
- *    
- *    Per acquisire l’input dell’utente usate la classe Scanner, come visto
- *       
- *        stamattina a lezione.
+ * calcolare il prezzo totale del viaggio, secondo queste regole: il prezzo del
+ * biglietto
+ * 
+ * è definito in base ai km (0.21 € al km) va applicato uno sconto del 20% per i
+ * minorenni
+ * 
+ * va applicato uno sconto del 40% per gli over 65
+ * 
+ * Per acquisire l’input dell’utente usate la classe Scanner, come visto
+ * 
+ * stamattina a lezione.
  */
- 
